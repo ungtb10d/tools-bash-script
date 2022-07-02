86c5e192c0e3081895ea04a5814ebda59587b295
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-07-02 18:09:10 -0500

    Create create-snapshot.sh

---
+++ create-snapshot.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+echo "Please run this code inside the pali-client folder"
+echo "Please type the region of the environment you want to create a snapshot of"
+read region
+echo "Please type the ticket number in this format DEPT-0000"
+read ticketnumber
+echo "Please type the environment name"
+read envname
+echo "Please wait snapshot is being created"
+pali --env-type prod --region $region env snapshot create --desc $ticketnumber $envname
+echo "Please enter the job id to check the status of snapshot"
+read jobid
+pali --env-type prod job status $jobid
+echo "Please type y to check the job status again"
+read answer
+if [ $answer == "y" ]; then
+    pali --env-type prod job status $jobid
+else
+    echo "You have chosen to exit, Goodbye"
+fi

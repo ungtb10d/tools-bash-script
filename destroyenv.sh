ee482cb9a355c5174a5f07a5af25a9b7f505f4e3
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-07-02 17:24:04 -0500

    Update destroyenv.sh

--- destroyenv.sh
+++ destroyenv.sh
@@ -17,11 +17,12 @@ env_name=`cat main.yml | grep -E 'env_name' | awk '{print $2}'`
 echo ${r}"Please type y if the above information is correct and you want to destroy $env_name environment"${rs}
 read answer
 if [ $answer == "y" ]; then
-    echo ${y}"Destroying environment $envname starting"${rs}
+    echo ${y}"Running destroy plan to verify if environment $envname ready for destruction"${rs}
+    sleep 1
     terragrunt plan-all -destroy
-    echo ${b}"If you see any error related to provider please type r to replace the provider in reporting and dgc folder"${rs}
+    echo ${b}"If you see any error related to module please type y to replace the module provider in reporting and dgc folder"${rs}
     read answer
-    if [ $answer == "r" ]; then
+    if [ $answer == "y" ]; then
         echo "Replacing provider, Please wait..."
         cd reporting/
         terragrunt state replace-provider 'registry.terraform.io/-/aws' 'registry.terraform.io/hashicorp/aws'
@@ -37,7 +38,7 @@ if [ $answer == "y" ]; then
         echo "Provider replaced returning to tmp folder"
         cd ..
         terragrunt plan-all -destroy
-        echo "If you do not see any issues please type y to destroy the environment"
+        echo ${r}"If you do not see any issues above please type y to destroy the environment, WARNING this will destroy the environment"${rs}
         read answer
         if [ $answer == "y" ]; then
             echo ${g}"Destroying environment $envname starting"${rs}

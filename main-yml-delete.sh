d5ef991786f1ea188ea1336f166a734a55918ac0
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-07-03 18:06:28 -0500

    Update main-yml-delete.sh

--- main-yml-delete.sh
+++ main-yml-delete.sh
@@ -28,7 +28,7 @@ if [ $answer == "y" ]; then
     git commit -m "removing main.yml after $ticketnumber"
     echo "Pushing the commit"
     git push --set-upstream origin $ticketnumber
-    echo ${y}"Commit successfuly pushed."${rs}
+    echo ${y}"Commit successfully pushed."${rs}
     echo ${y}"Now please go to github and create a pull request from $ticketnumber to master"${rs}
 
 else

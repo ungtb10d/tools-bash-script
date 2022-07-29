31a069f71a366b2be7ad08a48426e16246060e17
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-07-29 12:24:14 -0500

    Update upgrade-dgc.sh

--- upgrade-dgc.sh
+++ upgrade-dgc.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
+rs=`tput sgr0`    # reset
+g=`tput setaf 2`  # green
+y=`tput setaf 3`  # yellow
+r=`tput setaf 1`  # red
+b=`tput bold`     # bold
+u=`tput smul`     # underline
+nu=`tput rmul`    # no-underline
+
 echo "Please type the environment name you want to upgrade"
 read envname
 echo "Please type the environment version you want to upgrade to"
@@ -8,4 +16,14 @@ echo "Please type the ticket number in this format DEPT-00000"
 read ticketnumber
 echo "Please wait while we are upgrading your environment"
 pali --env-type prod env upgrade --version $envversion --zendesk-ticket $ticketnumber $envname
-echo "Upgrade completed"
+echo "Upgrade started"
+echo ${y}"Please enter the job id to check the status of job"${rs}
+read jobid
+pali --env-type prod job status $jobid
+echo ${y}"Please type y to check the job status again"${rs}
+read answer
+if [ $answer == "y" ]; then
+    pali --env-type prod job status $jobid
+else
+    echo ${g}"Exited, Goodbye!"${rs}
+fi

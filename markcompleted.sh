d5289c53949e07b1e6fb37f7efd3c86b0e5d6adb
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-08-28 15:03:14 -0500

    raw link added

--- markcompleted.sh
+++ markcompleted.sh
@@ -1,4 +1,4 @@
-for n in $(emirkoroglu/tools-bash-script/failed-ids.txt )
+for n in $(https://raw.githubusercontent.com/emirkoroglu/tools-bash-script/main/failed-ids.txt )
 do
     echo "Working on $n file name now"
     pali --env-type prod job mark-completed $n

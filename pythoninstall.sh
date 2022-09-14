27ca80b203249cbb87de0736a71a0f42530e1e5a
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-09-14 16:57:07 -0400

    Create pythoninstall.sh

---
+++ pythoninstall.sh
@@ -0,0 +1,11 @@
+pip3 install boto3
+python cog.py
+python3 -m pip install boto3
+python cog.py
+python -m pip install boto3
+pip install boto3
+python3 -m pip --version
+python -m pip install --upgrade pip
+python -m pip install boto3
+python cog.py
+yum install pip -y

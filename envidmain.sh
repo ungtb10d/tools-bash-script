aeeee992a54a10504b855e10f306a7e9e31d8f28
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-01 15:16:10 -0400

    destroytxt

--- envidmain.sh
+++ envidmain.sh
@@ -1,4 +1,13 @@
 
 echo "Please type the env name"
 read envname
-getenv $envname | grep -E 'Region|env_id|main.yml|env_deathdate|'
+
+
+curl https://raw.githubusercontent.com/emirkoroglu/tools-bash-script/main/environments.txt >> test.txt
+echo "Environments received. Gathering informations"
+while read p; do
+  getenv $p | grep -E 'Region|env_id|main.yml|env_deathdate|'
+done </home/ansible/test.txt
+echo "Deleting the test text file"
+sleep 1
+rm -rf /home/ansible/test.txt

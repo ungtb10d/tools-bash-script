6c592d2f267b5117e1f6ad8dd0588b7e3a65793b
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-01 13:52:50 -0400

    curl,delete added

--- checkenvlicense.sh
+++ checkenvlicense.sh
@@ -1,7 +1,11 @@
 
-
-wget
+echo "Getting the environment ids"
+sleep 1
+curl https://raw.githubusercontent.com/emirkoroglu/tools-bash-script/main/environments.txt >> test.txt
+echo "Environment ids received. Gathering license expiration dates"
 while read p; do
   aws s3 cp s3://collibra-main-dgc-licenses/$p/license.lic - | grep expirationDate
-done </Users/emir/REPOS/my-scripts/environments.txt
-
+done </home/ansible/test.txt
+echo "Deleting the test text file"
+sleep 1
+rm -rf /home/ansible/test.txt

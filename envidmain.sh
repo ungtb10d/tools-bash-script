354e94dcf5bc041b4f4e3c183ca246772f9a8782
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-01 15:27:17 -0400

    provider added

--- envidmain.sh
+++ envidmain.sh
@@ -3,7 +3,7 @@ echo "Getting the "
 curl https://raw.githubusercontent.com/emirkoroglu/tools-bash-script/main/destroyenv.txt >> test.txt
 echo "Environments received. Gathering informations"
 while read p; do
-  getenv $p | grep -E 'env_name|Region|env_id|main.yml|env_deathdate'
+  getenv $p | grep -E 'env_name|Region|env_id|main.yml|env_deathdate|Cloud_Provider'
 done </home/ansible/test.txt
 echo "Deleting the test text file"
 sleep 1

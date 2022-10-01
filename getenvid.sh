b806be2dd75a8136e0a47a18c66e90212ba15b07
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-01 16:01:59 -0400

    fixes

---
+++ getenvid.sh
@@ -0,0 +1,10 @@
+
+echo "Getting the environments list "
+curl https://raw.githubusercontent.com/emirkoroglu/tools-bash-script/main/licensecheckenvs.txt >> test.txt
+echo "Environments received. Gathering informations"
+while read p; do
+  getenv $p | grep -E 'env_id'
+done </home/ansible/test.txt
+echo "Deleting the test text file"
+sleep 1
+rm -rf /home/ansible/test.txt

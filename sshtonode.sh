1bca3fc320826efdd4ae9ea20a7aff1fa7f16a54
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-24 16:13:37 -0500

    Update sshtonode.sh

--- sshtonode.sh
+++ sshtonode.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo "Please type the instanceid to find out the instance name"
-read instanceidname
+read instanceid
 cat ~/ssh.main.config | grep $instanceidname
 echo "Please type the instance region"
 read region
-echo "Please type the instance id"
-read instanceid
 sshprod ng_proxy-nirvana-$region-$instanceid
 
 

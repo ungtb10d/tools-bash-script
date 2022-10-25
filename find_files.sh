1e87a64c94341cf11b7cda39031259e9227ceb78
Emir Koroglu <69024539+emirkoroglu@users.noreply.github.com>
2022-10-25 15:57:00 -0500

    colors added

--- find_files.sh
+++ find_files.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-echo "Please type the folder that you want to remove files"
+rs=`tput sgr0`    # reset
+g=`tput setaf 2`  # green
+y=`tput setaf 3`  # yellow
+r=`tput setaf 1`  # red
+b=`tput bold`     # bold
+u=`tput smul`     # underline
+nu=`tput rmul`    # no-underline
+
+
+echo ${b}"Please type the folder that you want to remove files"${rs}
 read folder
-echo "Remove files older than how many days? (Please type the number only)"
+echo ${y}"Remove files older than how many days? (Please type the number only)"${rs}
 read number
-echo "Are you sure you want to delete files from $folder older than $number days? Please type yes to continue"
+echo ${r}"Are you sure you want to delete files from $folder older than $number days? Please type yes to continue"${rs}
 read answer
 if [ $answer == "yes" ]; then
-    echo "Remove operation starting"
+    echo ${g}"Remove operation starting"${rs}
     sleep 2
     find $folder* -mtime +$number -exec rm {} \;
-    echo "Files in $folder older than $number days have been removed"
-    echo "Please type yes to verify that no files exist older than $number days in the folder"
+    echo ${g}"Files in $folder older than $number days have been removed"${rs}
+    echo ${b}"Please type yes to verify that no files exist older than $number days in the $folder"{rs}
     if [ $answer == "yes" ]; then
        find $folder -mtime +$number -print
     else
        2> /dev/null
     fi
 else
-    echo "You have chosen to exit, Goodbye!"
+    echo ${b}"You have chosen to exit, Goodbye!"{rs}
 fi
 
 
